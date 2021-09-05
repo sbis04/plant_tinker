c599cc64506bf6ff8edf74396340e80018533196
Souvik Biswas <sbis1999@gmail.com>
2021-09-05 20:55:10 +0530

    Update scroll physics

--- lib/screens/dashboard_page.dart
+++ lib/screens/dashboard_page.dart
@@ -72,6 +72,7 @@ class _DashboardPageState extends State<DashboardPage> {
                   right: 16.0,
                 ),
                 child: ListView(
+                  physics: BouncingScrollPhysics(),
                   children: [
                     Stack(
                       children: [
