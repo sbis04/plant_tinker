112d58aac972763bf7ecef2ee334f43f53476752
Souvik Biswas <sbis1999@gmail.com>
2021-09-05 20:04:34 +0530

    Add new colors

--- lib/res/palette.dart
+++ lib/res/palette.dart
@@ -4,4 +4,6 @@ class Palette {
   static const Color neon_green = Color(0xFF51ca98);
   static const Color blue_gray = Color(0xFF1a3045);
   static const Color blue_gray_dark = Color(0xFF142637);
+  static const Color blue_accent = Color(0xff23b6e6);
+  static const Color green_accent = Color(0xff02d39a);
 }
